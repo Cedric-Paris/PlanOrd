e9015a525b521b757f936a8a7201cc3526c8bde2
Cedric-Paris <cedric.paris@etu.utc.fr>
2018-03-21 12:14:13 +0100

    Initial commit

---
+++ PlanOrd/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace PlanOrd
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Start(object sender, StartupEventArgs e)
+        {
+            MainWindow wnd = new MainWindow();
+            wnd.Show();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# PlanOrd
+
+(Msagl folder contains sources forked from [Microsoft MSAGL](https://github.com/Microsoft/automatic-graph-layout) used to render the graph)
