993aab12a5c9fd5a6dba1460ee58244ef07cb4ff
tboulair <theo.boulaire@free.fr>
2018-05-08 17:49:01 +0200

    Introduction du parser et tests unitaires.

---
+++ PlanOrd/Services/JsonPlanProvider.cs
@@ -0,0 +1,83 @@
+﻿using Newtonsoft.Json;
+using PlanOrd.Model;
+using System;
+using System.Collections.Generic;
+
+namespace PlanOrd.Services
+{
+    public class JsonPlanProvider : IPlanProvider
+    {
+        public Plan Plan { get; private set; }
+
+        public event EventHandler PlanReady;
+
+        /// <summary>
+        /// Creer un Plan a partir d'une chaine Json et le stocker dans le JsonPlanProvider.
+        /// </summary>
+        /// <param name="value">Chaine Json representant le plan</param>
+        public void CreatePlanFromString(string value)
+        {
+            var jsonPlan = JsonConvert.DeserializeObject<JsonPlan>(value);
+            var plan = new Plan();
+            foreach (JsonNode jNode in jsonPlan.Nodes)
+            {
+                PlanNode pn = new PlanNode(jNode.Id);
+                pn.Duration = jNode.Parameters.Duration;
+                pn.Label = jNode.Parameters.Label;
+                plan.Graph.Nodes.Add(jNode.Id, pn);
+
+                foreach(string crit in jNode.Criteria.Keys)
+                {
+                    if (plan.GlobalCharacteristics.ContainsKey(crit))
+                        plan.GlobalCharacteristics[crit] += jNode.Criteria[crit];
+                    else
+                        plan.GlobalCharacteristics.Add(crit, jNode.Criteria[crit]);                       
+                }
+            }
+            foreach (JsonEdge e in jsonPlan.Edges)
+            {
+                plan.Graph.CreateArc(e.SourceId, e.TargetId);
+            }
+
+            Plan = plan;
+            if (PlanReady != null)
+                PlanReady(this, EventArgs.Empty);
+        }
+    }
+
+    /// <summary>
+    /// Classe support pour la deserialization
+    /// </summary>
+    internal class JsonPlan
+    {
+        public List <JsonNode> Nodes { get; set; }
+        public List <JsonEdge> Edges { get; set; }
+    }
+
+    internal class JsonNode
+    {
+        [JsonProperty(PropertyName = "v")]
+        public int Id { get; set; }
+
+        public Dictionary<string, int> Criteria { get; set; }
+
+        [JsonProperty(PropertyName = "params")]
+        public JsonNodeParams Parameters { get; set; }
+    }
+
+    internal class JsonEdge
+    {
+        [JsonProperty(PropertyName = "v")]
+        public int SourceId { get; set; }
+        [JsonProperty(PropertyName = "w")]
+        public int TargetId { get; set; }
+    }
+
+    internal class JsonNodeParams
+    {
+        public bool Controlled { get; set; }
+        public int Duration { get; set; }
+        public string Label { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ PlanOrdTests/JsonUnitTest.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlanOrd.Model;
+using PlanOrd.Services;
+using System.IO;
+
+namespace PlanOrdTests
+{
+    [TestClass]
+    public class JsonUnitTest
+    {
+        [TestMethod]
+        public void CreatePlanFromStringTest()
+        {
+            bool eventRaised = false;
+            string json = File.ReadAllText(@"./../../plan.json");
+
+            JsonPlanProvider pp = new JsonPlanProvider();
+            pp.PlanReady += (s, e) => { eventRaised = true; };
+            pp.CreatePlanFromString(json);
+
+            Plan plan = pp.Plan;
+            Assert.IsTrue(eventRaised);
+            Assert.IsNotNull(plan);
+
+            // Tests sur les caracs
+            Assert.AreEqual(plan.GlobalCharacteristics.Count, 1);
+            Assert.AreEqual(plan.GlobalCharacteristics["difficulty"], 1);
+
+            // Tests sur le graphe
+            for (int i = 0; i < 6; i++)
+            {
+                Assert.AreEqual(plan.Graph.Nodes[i].Id, i);
+            }
+            Assert.AreEqual(plan.Graph.Nodes[0].Duration, 0);
+            Assert.AreEqual(plan.Graph.Nodes[0].Fathers.Count, 0);
+            Assert.AreEqual(plan.Graph.Nodes[0].Children.Count, 1);
+            Assert.AreEqual(plan.Graph.Nodes[0].Children[1].Label, "CallMedivac()");
+            Assert.AreEqual(plan.Graph.Nodes[0].Label, "Start");
+
+            Assert.AreEqual(plan.Graph.Nodes[2].Duration, 1);
+            Assert.AreEqual(plan.Graph.Nodes[2].Fathers.Count, 1);
+            Assert.AreEqual(plan.Graph.Nodes[2].Children.Count, 1);
+            Assert.AreEqual(plan.Graph.Nodes[2].Label, "add_victim()");
+
+            Assert.AreEqual(plan.Graph.Nodes[5].Duration, 1);
+            Assert.AreEqual(plan.Graph.Nodes[5].Fathers.Count, 1);
+            Assert.AreEqual(plan.Graph.Nodes[5].Fathers[4].Label, "MedivacLands()");
+            Assert.AreEqual(plan.Graph.Nodes[5].Children.Count, 0);
+            Assert.AreEqual(plan.Graph.Nodes[5].Label, "EvacuateVictims()");
+        }
+    }
+}
